0facd5607c2acfeb1338eaaf5b08c6d6fa7b0a1c
Leandro Castillo Borja <leccbo1995@gmail.com>
2020-05-24 02:28:50 -0500

    Crear abstract de aula

---
+++ app/src/main/java/com/leandro1995/kotlinpoo/abstract/Classroom.kt
@@ -0,0 +1,11 @@
+package com.leandro1995.kotlinpoo.abstract
+
+abstract class Classroom constructor(
+    var name: String,
+    var surname: String,
+    var age: Int,
+    var year: Int,
+    var type: String
+) {
+
+}
